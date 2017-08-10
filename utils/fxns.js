61504b296e0576aeab50db55d8f4d5903dee983e
lawshe <jessica@jlawshe.com>
2017-08-10 11:08:31 -0500

    Add spell check function
    Words misspelled are returned as keys in an object and values are
    arrays, containing indices where words occur in user input

--- utils/fxns.js
+++ utils/fxns.js
@@ -1,3 +1,42 @@
-export default {
+import basic from '../constants/basic-words';
 
+const allBasicWords = basic.operations.concat(basic.things, basic.qualities);
+
+const wordsToObj = (arr) => {
+  // create an object where keys are words from input text and values are arrays, containing indices where words occur
+  var wordsObj = {};
+  arr.forEach((word, idx) => {
+    var w = word.toLowerCase();
+    if (!wordsObj[w]) {
+      wordsObj[w] = [idx];
+    } else {
+      wordsObj[w].push(idx);
+    }
+  });
+  return wordsObj;
+}
+
+const makeArray = (str) => {
+  return str ? str.match(/\w+/g) : [];
+}
+
+const spellCheck = (str) => {
+  // Using BASIC English to spell check.
+  var misspelled = {};
+  if (str) {
+    var strArr = makeArray(str);
+    var wordsObj = wordsToObj(strArr);
+    // Object keys are words from user input
+    for (var word in wordsObj) {
+      if(allBasicWords.indexOf(word) === -1) {
+        misspelled[word] = wordsObj[word];
+      }
+    }
+  }
+  return misspelled;
+}
+
+export default {
+  makeArray,
+  spellCheck
 }
