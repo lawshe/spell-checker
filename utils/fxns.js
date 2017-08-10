5f55ffaa686527cf6c1345b8fdd4f2c046c15880
lawshe <jessica@jlawshe.com>
2017-08-10 11:35:28 -0500

    Spell check - Handle plural things

--- utils/fxns.js
+++ utils/fxns.js
@@ -21,7 +21,7 @@ const makeArray = (str) => {
 }
 
 const spellCheck = (str) => {
-  // Using BASIC English to spell check.
+  // Using BASIC English to spell check. Also considering plural cases on `basic.things` words
   var misspelled = {};
   if (str) {
     var strArr = makeArray(str);
@@ -29,7 +29,14 @@ const spellCheck = (str) => {
     // Object keys are words from user input
     for (var word in wordsObj) {
       if(allBasicWords.indexOf(word) === -1) {
-        misspelled[word] = wordsObj[word];
+        // check if plural thing
+        if (word.slice(-1) === 's' && basic.things.indexOf(word.slice(0, -1)) !== -1) {
+        } else if (word.slice(-3) === 'ies' && basic.things.indexOf(word.slice(0, -3) + 'y') !== -1) {
+        } else if (word.slice(-2) === 'es' && basic.things.indexOf(word.slice(0, -2)) !== -1) {
+        } else{
+          // not in list and not plural
+          misspelled[word] = wordsObj[word];
+        }
       }
     }
   }
