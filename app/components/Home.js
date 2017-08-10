65257fd903e32093fe8e087179e7de22d6cebd4c
lawshe <jessica@jlawshe.com>
2017-08-10 10:42:28 -0500

    Import text input component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Intro from 'prtls/Intro.js';
+import TextInput from 'prtls/TextInput.js';
 import glob from 'style';
 /**
   *
@@ -26,6 +27,11 @@ class Home extends React.Component {
     return (
       <div>
         <Intro parallax={this.props.parallax} />
+        <div className="row">
+          <div className="col s6">
+            <TextInput />
+          </div>
+        </div>
       </div>
     )
   }
