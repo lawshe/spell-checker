85ca95a37170e86a6c98dfeda54fe7f0d25ccf6c
lawshe <jessica@jlawshe.com>
2017-08-10 11:23:38 -0500

    Add TextOutput component and `.pad_lg` to parent

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Intro from 'prtls/Intro.js';
 import TextInput from 'prtls/TextInput.js';
+import TextOutput from 'prtls/TextOutput.js';
 import glob from 'style';
 /**
   *
@@ -27,9 +28,12 @@ class Home extends React.Component {
     return (
       <div>
         <Intro parallax={this.props.parallax} />
-        <div className="row">
+        <div className={`row container ${glob.pad_lg}`}>
           <div className="col s6">
             <TextInput />
+          </div>
+          <div className="col s6">
+            <TextOutput />
           </div>
         </div>
       </div>
