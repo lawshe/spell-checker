0d71d1820e6fd7564e2eded83417083641918092
lawshe <jessica@jlawshe.com>
2017-08-10 10:44:45 -0500

    Add semicolon

--- app/components/partials/TextInput.js
+++ app/components/partials/TextInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateText } from '../../actions/actionCreators'
+import { updateText } from '../../actions/actionCreators';
 import glob from 'style';
 
 /**
