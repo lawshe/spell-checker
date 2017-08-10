e9ba7a9b9cc2f517170edfd0d67e6ef3844287c1
lawshe <jessica@jlawshe.com>
2017-08-10 11:11:36 -0500

    Extend UPDATE_TEXT to spell check
    Extend reducer to update `input_text_arr ` and `misspelled ` in state.
    
    `misspelled` is the object of misspelled words
    
    `input_text_arr` is the user input split into an array

--- app/reducers/spellingReducers.js
+++ app/reducers/spellingReducers.js
@@ -1,13 +1,24 @@
 import { UPDATE_TEXT } from '../actions/actionTypes';
+import fxns from 'fxns';
 
 const initialState = {
-  input_text: ''
+  input_text: '', // User input
+  input_text_arr: [],  // Array of input text - used in JSX in TextOutput
+  misspelled: {} // Keys are words from user input and values are arrays of indices where word occurs
 };
 
 export default function accessibilityReducers(state = initialState, action) {
   switch (action.type) {
     case UPDATE_TEXT: {
-      return Object.assign({}, state, { input_text: action.input_text });
+      let misspelled = {};
+      let input_text_arr = [];
+
+      if (action.input_text) {
+        misspelled = fxns.spellCheck(action.input_text);
+        input_text_arr = fxns.makeArray(action.input_text);
+      }
+
+      return Object.assign({}, state, { input_text: action.input_text, input_text_arr, misspelled });
     }
     default:
       return state;
