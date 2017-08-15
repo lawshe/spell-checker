792c7afb15c11c0d8a67d4cd1643e18fa70b39dc
lawshe <jessica@jlawshe.com>
2017-08-15 17:13:08 -0500

    new isPural(); Added spell check exception
    Move checking if word is plural to function isPlural()
    
    Added an exception, the first word in the user input can be capitalized

--- utils/fxns.js
+++ utils/fxns.js
@@ -10,8 +10,23 @@ const makeArray = (str) => {
   return str ? str.split(' ') : [];
 }
 
+const isPlural = (word) => {
+  let result = false;
+
+  if (word.slice(-1) === 's' && basic.things.indexOf(word.slice(0, -1)) !== -1) {
+    result = true;
+  } else if (word.slice(-3) === 'ies' && basic.things.indexOf(word.slice(0, -3) + 'y') !== -1) {
+    result = true;
+  } else if (word.slice(-2) === 'es' && basic.things.indexOf(word.slice(0, -2)) !== -1) {
+    result = true;
+  }
+
+  return result;
+}
+
 const spellCheck = (str) => {
-  // Using BASIC English to spell check. Also considering plural cases on `basic.things` words
+  // Using BASIC English to spell check.
+  // Also considering plural cases on `basic.things` words
   let misspelled = {};
   if (str) {
     const strArr = makeArray(str);
@@ -19,19 +34,24 @@ const spellCheck = (str) => {
     for (var word of inputSet) {
       let w = removePunctuation(word);
       if (!misspelled[w]) {
-        if (allBasicWords.indexOf(w) === -1){
-          // check if plural thing
-          if (w.slice(-1) === 's' && basic.things.indexOf(w.slice(0, -1)) !== -1) {
-          } else if (w.slice(-3) === 'ies' && basic.things.indexOf(w.slice(0, -3) + 'y') !== -1) {
-          } else if (w.slice(-2) === 'es' && basic.things.indexOf(w.slice(0, -2)) !== -1) {
-          } else{
-            // not in list and not plural
-            misspelled[w] = true;
-          }
+        if (allBasicWords.indexOf(w) === -1 && !isPlural(w)){
+          misspelled[w] = true;
+        }
+      }
+    }
+
+    // Exceptions
+    if (Object.keys(misspelled).length) {
+      // First word in input can be capitalized
+      if (misspelled[strArr[0]] && strArr[0][0] === strArr[0][0].toUpperCase()) {
+        // Word is capitalized. Spell check lowercase version.
+        if (allBasicWords.indexOf(strArr[0].toLowerCase()) != -1 || isPlural(strArr[0].toLowerCase())) {
+          delete misspelled[strArr[0]];
         }
       }
     }
   }
+
   return misspelled;
 }
 
